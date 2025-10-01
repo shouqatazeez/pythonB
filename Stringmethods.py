1e13990f90911a58c72e51ea63f8057605917d88
Shouqat Azeez <mdshouqatazeez@gmail.com>
2025-10-02 01:03:46 +0530

    work with some of the string method in python

---
+++ Stringmethods.py
@@ -0,0 +1,13 @@
+name = "shouqat Azeez   "
+
+# this method is used to convert the string value into the upper case
+print(name.upper())
+print(name.lower())  # it converts the all charcters in to  the lower case
+# this method is used to covert the first charcter in the upper case
+print(name.title())
+# this method is used to find the index value of the particular charcter in the string
+print(name.find("ou"))
+name = "        Shouqat Azeez   "
+
+# it is used to remove the spaces in the string value at the starting or at the last or at the  also
+print(name.strip())
