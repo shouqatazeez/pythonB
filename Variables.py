af1d71ebbe989405d4f7d104e26082cce961e37a
Shouqat Azeez <mdshouqatazeez@gmail.com>
2025-10-02 00:45:52 +0530

    discuss with variables

---
+++ Variables.py
@@ -0,0 +1,6 @@
+num = 12  # This is how we declare the number we don't want to use the like data type
+# if we declare the value in between the double quotes so that we can get an idea of we declare the string
+name = "shouqat Azeez MOhammad"
+
+is_loggedin = True  # this is how we declare the boolean value
+nums = 34.5  # this is how we declare the float value
